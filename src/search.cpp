02796e58bb0b95dfb0343a32059dc2dde2fba3a2
Sebastian Sole <scsole@protonmail.com>
2021-04-16 16:59:21 +1200

    Load docnos into memory

--- src/search.cpp
+++ src/search.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
+#include <vector>
 
-int main(int argc, char** argv) {
-    std::string line;
-    std::ifstream infile ("docnos.bin");
+char* _prog; // Program name
 
+std::vector<std::string> docnos; // TREC DOCNOs
+
+/**
+ * Load index from disk into memory.
+ */
+void load_index() {
+    std::ifstream infile;   // Input file
+    char buf[1024];         // Input buffer
+
+    infile.open("docnos.bin");
     if (infile.is_open()) {
-        while (std::getline(infile, line)) {
-            std::cout << line << '\n';
+        while (infile.getline(buf, sizeof(buf))) {
+            docnos.push_back(std::string(buf));
         }
         infile.close();
-    } else
-        std::cout << "Unable to open file\n";
+    } else {
+        std::cerr << _prog << ": unable to open docnos.bin\n";
+        exit(1);
+    }
+}
+
+int main(int argc, char** argv) {
+    _prog = argv[0];
+
+    load_index();
+
+    std::cout << docnos.size() << '\n';
 
     return 0;
 }
